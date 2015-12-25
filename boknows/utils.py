eb33c792f73980f7f938f2653d9abe3dd2ca7fc5
Neil Bedi <neilbedi@gmail.com>
2015-12-25 15:48:24 -0500

    merge csvs instead of outputing multiple csvs

--- boknows/utils.py
+++ boknows/utils.py
@@ -1,45 +1,31 @@
 import os
+import re
 import requests
+import pandas as pd
+from io import StringIO
 
 url = 'http://web1.ncaa.org/stats/StatsSrv/rankings'
 
-def csv_dump(dir_path='dump', sport_code='MBB', academic_year='2015', rpt_weeks='141', div='1', stat_seq='-103'):
+def csv_dump(dir_path='dump', sport_code='MBB', academic_year='2015', rpt_weeks='141', div='1', stat_seq='-101'):
     """
-    Starting point. Dumps an unparsed, messy csv. Most inputs are based on 
+    Dumps a csv file according to inputs to specified path. Most inputs are based on 
     arbitrary NCAA codes that users should not have to know (except for maybe 
     sport_code).
     
     :param dir_path:
         Path to directory to dump CSV files in. Defaults to directory called 'dump'
-    :param sport_code:
-        NCAA code for desired sport. Defaults to Men's Basketball.
-    :param academic_year:
-        Four digit academic year. Defaults to 2015.
-    :param rpt_weeks:
-        NCAA code for end week of returned stats. Defaults to end of 12/9/2015.
-    :param div:
-        NCAA division. Defaults to 1.
-    :param stat_seq:
-        NCAA code for specific stats requested. Defaults to all team stats.
+    
+    Other inputs documented in :method:`.ncaa_request`.
     """
-    payload = { 'sportCode': sport_code,
-                'academicYear': academic_year,
-                'rptType': 'CSV',
-                'rptWeeks': rpt_weeks,
-                'div': div,
-                'statSeq': stat_seq,
-                'doWhat': 'showrankings'
-                }
-                
-    r = requests.post(url, payload)
-    files = csv_cleanup(r.content)
-
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    csv = csv_cleanup(ncaa_request('CSV', sport_code, academic_year, rpt_weeks, div, stat_seq))
+    
+    path = dir_path + '/' + sport_code + '/div' + div
+        
+    if not os.path.exists(path):
+        os.makedirs(path)
             
-    for key in files:
-        with open(dir_path+'/'+key+'.csv', 'w') as f:
-            f.write(files[key])
+    with open(path+'/'+academic_year+'.csv', 'w') as f:
+        f.write(csv)
 
 def csv_cleanup(content=None):
     """
@@ -52,6 +38,7 @@ def csv_cleanup(content=None):
         Original output from NCAA stats
     """
     files = {}
+    csvs = []
     
     if content is None:
         return files
@@ -59,10 +46,43 @@ def csv_cleanup(content=None):
     key = ''
     for line in content.split('\n'):
         if 'Division' in line:
-            key = line.replace(' ', '')
+            key = re.sub(r'DivisionI*', '', line.replace(' ', ''))
             files[key] = ''
         if ',' in line:
             files[key] = files[key] + line + '\n'
+            
+    for value in files.values():
+        csvs.append(pd.read_csv(StringIO(value.decode('utf-8'))))
     
-    return files
+    merged = reduce(lambda left,right: pd.merge(left, right[right.columns.difference(left.columns.difference(['Name']))], on='Name'), csvs)
+    return merged.to_csv()
+
+def ncaa_request(rpt_type, sport_code, academic_year, rpt_weeks, div, stat_seq):
+    """
+    Makes request to NCAA web application at http://web1.ncaa.org/stats/StatsSrv/rankings?. 
+    
+    :param rpt_type:
+        Format of response. Can be HTML, ASCII, PDF or CSV.
+    :param sport_code:
+        NCAA code for desired sport.
+    :param academic_year:
+        Four digit academic year. Earliest input is 2002.
+    :param rpt_weeks:
+        NCAA code for end week of returned stats.
+    :param div:
+        NCAA division.
+    :param stat_seq:
+        NCAA code for specific stats requested.
+    """
+    payload = { 'sportCode': sport_code,
+                'academicYear': academic_year,
+                'rptType': rpt_type,
+                'rptWeeks': rpt_weeks,
+                'div': div,
+                'statSeq': stat_seq,
+                'doWhat': 'showrankings'
+                }
+                
+    r = requests.post(url, payload)
+    return r.content
     
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,12 +6,9 @@ class TestUtils(unittest.TestCase):
     def test_csv_cleanup(self):
         self.maxDiff = None
         files = {}
-        expected = {
-            'DivisionIWon-LostPercentage': '"Rank","Name","W","L","Pct"\n"1","Michigan St.","10","0","100.0"\n',
-            'DivisionIScoringOffense': '"Rank","Name","GM","W-L","PTS","PPG"\n"1","The Citadel","10","6-4","936","93.6"\n"2","La.-Lafayette","6","3-3","551","91.8"\n'
-        }
         
         with open('examples/test.csv', 'r') as f:
             files = utils.csv_cleanup(f.read())
         
-        self.assertEqual(files, expected)
+            with open('examples/expected.csv', 'r') as f:
+                self.assertEqual(files, f.read())
