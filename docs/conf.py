322de2a79764e85cfe227f03fa2006e2706acd3c
Neil Bedi <neilbedi@gmail.com>
2015-12-26 00:39:47 -0500

    adding sphinxcontrib-httpdomain for autoflask

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinxcontrib.autohttp.flask'
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.autohttp.flask'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
