318709e1614160cb7a48a0ff15b7cfd0179394e2
Neil Bedi <neilbedi@gmail.com>
2015-12-26 00:32:47 -0500

    force column sort to handle unexpected sorting in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,5 +17,7 @@ class TestUtils(unittest.TestCase):
         
         with open('examples/test.csv', 'r') as f:
             files = utils.csv_cleanup(f.read())
+            frame1 = pd.read_csv(StringIO(files[1])).sort_index(axis=1)
+            frame2 = pd.read_csv(open('examples/expected.csv')).sort_index(axis=1)
             
-            assert_frame_equal(pd.read_csv(StringIO(files[1])), pd.read_csv(open('examples/expected.csv')))
+            assert_frame_equal(frame1, frame2)
